b700cad9b6da58812506f7a09a45cf4c5a4db4e5
goriiiii <n05070606@gmail.com>
2023-11-25 23:50:12 +0700

    book section of admin done

--- admin/ad-product.js
+++ admin/ad-product.js
@@ -4,24 +4,31 @@ let data = '';
 function bookSort() {
   let holder = document.querySelector(".report-container");
   let list = document.querySelector(".booktype");
-  data = `
+  data = ` 
   <div class="book-upload">
+
   <div class="upload-btn"><button class="upload-btn" onclick="showUploadForm()">Upload</button></div>
   <div class="pop-up-upload-form">
-  
+  <label for="book-upload-id">Book's ID:</label>
+  <input type="text" id="book-upload-id" name="book-upload-id">
   <label for="book-upload-name">Book's title:</label>
   <input type="text" id="book-upload-name" name="book-upload-name">
   <label for="book-upload-price">Book's price:</label>
   <input type="text" id="book-upload-price" name="book-upload-price">
-  <label for="book-upload-id">Book's ID:</label>
-  <input type="text" id="book-upload-id" name="book-upload-id">
+   <label for="book-upload-type">Book's category:</label>
+  <input type="text" id="book-upload-type" name="book-upload-type">
+   <label for="book-upload-author">Book's author:</label>
+  <input type="text" id="book-upload-author" name="book-upload-author">
+   <label for="book-upload-publisher">Book's publisher:</label>
+  <input type="text" id="book-upload-publisher" name="book-upload-publisher">
   <label for="myfile">Select a file:</label>
-  <input type="file" id="myfile" name="myfile">
+  <input type="file" id="myfile" name="myfile" accept="image/*" onchange="handleImageUpload(event)">
+  <div class="image-container"></div>
   <div class="upload-form-btn">
-  <button onclick="saveBookToLocalStorage()">Save</button>
+  <button onclick="validateAndSave()">Save</button>
   <button class="cancel-upload" onclick="closeUploadForm()">Cancel</button>
   </div>
-</div>
+  </div>
 </div>
     <div class="book-category">
       <select id="categoryDropdown">
@@ -35,7 +42,7 @@ function bookSort() {
   holder.innerHTML = ' ';
   list.innerHTML = data;
 
-  // Get the dropdown element after adding it to the DOM
+
   let dropdown = document.getElementById('categoryDropdown');
 
   dropdown.addEventListener('change', function () {
@@ -49,17 +56,17 @@ function displayProducts(category) {
   let categoryBooks = arr.filter(product => product.categories === selectedCategory);
 
   let categoryContainer = document.querySelector(".report-container");
-  categoryContainer.innerHTML = ''; // Clear existing content
+  categoryContainer.innerHTML = '';
 
   categoryBooks.forEach(book => {
-    // Creating a div element to hold the book details and delete button
+
     const bookDiv = document.createElement('div');
     bookDiv.classList.add('book-row');
 
     const bookContent = `
       <div class="book-row-content">
         <div class="book-image">
-          <img src="../${book.img}" alt="${book.name}">
+          <img src="${book.img}" alt="${book.name}">
         </div>
         <div class="book-category">${book.categories}</div>
         <div class="book-title">${book.name}</div>
@@ -69,7 +76,7 @@ function displayProducts(category) {
 
     bookDiv.innerHTML = bookContent;
 
-    // Appending bookDiv to the category container
+
     categoryContainer.appendChild(bookDiv);
   });
 
@@ -81,52 +88,86 @@ function displayProducts(category) {
       if (confirmed) {
         const bookRow = event.target.parentElement.parentElement;
 
-        // Find the index of the book in the array by its name or ID
         const indexToRemove = arr.findIndex(product => product.name === bookRow.querySelector('.book-title').textContent);
         if (indexToRemove !== -1) {
           arr.splice(indexToRemove, 1);
 
-          // Update the localStorage with the modified array
           localStorage.setItem('List-products', JSON.stringify(arr));
 
-          // Remove the book from the display
           bookRow.remove();
-          // Add logic here to delete the book data from your storage (localStorage, database, etc.)
         }
       }
     });
   });
 }
-function handleImageUpload() {
-  const uploadBtn = document.querySelector('.upload-book-btn');
+function handleImageUpload(event) {
+  const uploadedImage = event.target.files[0];
+  const reader = new FileReader();
 
-  uploadBtn.addEventListener('click', function () {
-    const fileInput = document.createElement('input');
-    fileInput.type = 'file';
-    fileInput.accept = 'image/*'; // Accept only image files
+  reader.onload = function (e) {
+    const imgElement = document.createElement('img');
+    imgElement.src = e.target.result;
 
-    fileInput.addEventListener('change', function (event) {
-      const file = event.target.files[0];
-      if (file) {
-        // Handle the uploaded file here, for example, display a preview:
-        const reader = new FileReader();
-        reader.onload = function (event) {
-          const imgPreview = document.createElement('img');
-          imgPreview.src = event.target.result;
-          document.body.appendChild(imgPreview);
-        };
-        reader.readAsDataURL(file);
-      }
-    });
 
-    // Trigger the file input click to open the file selection dialog
-    fileInput.click();
-  });
+    imgElement.style.maxWidth = '100%';
+    imgElement.style.height = 'auto';
+
+    const imageContainer = document.querySelector('.image-container');
+    imageContainer.innerHTML = '';
+    imageContainer.appendChild(imgElement);
+  };
+
+  reader.readAsDataURL(uploadedImage);
 }
 
-// Helper function to generate a unique product ID
-function generateProductId() {
-  return Math.floor(Math.random() * 100000) + 1;
+function validateAndSave() {
+
+  var bookId = document.getElementById('book-upload-id').value.trim();
+  var bookTitle = document.getElementById('book-upload-name').value.trim();
+  var bookPrice = document.getElementById('book-upload-price').value.trim();
+  var bookCategory = document.getElementById('book-upload-type').value.trim();
+  var bookAuthor = document.getElementById('book-upload-author').value.trim();
+  var bookPublisher = document.getElementById('book-upload-publisher').value.trim();
+  var Regex = /^\d+$/; // 
+  var isValidPrice = Regex.test(bookPrice);
+  var isValidID = Regex.test(bookId);
+
+  if (bookId === '' || bookTitle === '' || bookPrice === '' || bookCategory === '' || bookAuthor === '' || bookPublisher === '') {
+    alert('Please fill in all fields.');
+    return;
+  }
+  if (!isValidPrice) {
+    alert('Please enter a valid price');
+    return;
+  }
+  if (!isValidID) {
+    alert('Please enter a valid ID');
+    return;
+  }
+  const imgElement = document.querySelector('.image-container img');
+  const imageData = imgElement ? imgElement.src : '';
+  const product = {
+    productId: bookId,
+    categories: bookCategory,
+    author: bookAuthor,
+    publishingHouse: bookPublisher,
+    img: imageData,
+    name: bookTitle,
+    price: bookPrice
+  }
+  saveProduct(product);
+  alert('Product added');
+
+}
+
+function saveProduct(product) {
+
+  let products = JSON.parse(localStorage.getItem('List-products')) || [];
+
+
+  products.push(product);
+
+  localStorage.setItem('List-products', JSON.stringify(products));
 }
 
 function showUploadForm() {
