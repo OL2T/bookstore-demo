5ca850fa83bbabac4b9267d2d93e7b182ecc1e71
nghi <nghihuynh1210@gmail.com>
2023-11-28 19:42:02 +0700

    update customer management

--- admin/list-users.js
+++ admin/list-users.js
@@ -67,7 +67,7 @@ function displayCustomerList() {
 function deleteUser(index) {
   const user = listUsers[index];
   const confirmation = confirm('Are you sure you want to delete this user?');
-  
+
   if (confirmation) {
     listUsers.splice(index, 1);
     localStorage.setItem('List-users', JSON.stringify(listUsers));
@@ -75,29 +75,111 @@ function deleteUser(index) {
   }
 }
 
+function openModal() {
+  const editUserModal = document.getElementById('editUserModal');
+  if (editUserModal) {
+    editUserModal.style.display = 'block';
+  }
+}
+
+function closeModal() {
+  const editUserModal = document.getElementById('editUserModal');
+  editUserModal.style.display = 'none';
+}
+
+function updateUser(event, index) {
+  event.preventDefault();
+  let user = listUsers[index];
+  const name = document.getElementById('nameInput').getAttribute('value');
+  const username = document.getElementById('usernameInput').getAttribute('value');
+  const phone = document.getElementById('phoneInput').getAttribute('value');
+  const email = document.getElementById('emailInput').getAttribute('value');
+  const role = document.getElementById('roleInput').getAttribute('value');
+
+  user.fullName = name;
+  user.username = username;
+  user.phone = phone;
+  user.email = email;
+  user.role = role;
+
+  localStorage.setItem('List-users', JSON.stringify(listUsers));
+
+  closeModal();
+  displayCustomerList();
+}
+
+function handleChangeValue(event, inputId) {
+  event.preventDefault();
+  const input = document.getElementById(inputId);
+  input.setAttribute('value', event.target.value);
+}
+
 function editUser(index) {
+  let user = listUsers[index];
+  const modal = document.createElement('div');
+  modal.classList.add('modal');
+  modal.id = 'editUserModal';
+  const modalContent = document.createElement('div');
+  modalContent.classList.add('modal-content');
+  modalContent.innerHTML = `
+    <form id="editUserForm">
+      <div class="modal-header">
+        <div class="edit-header">Edit User</div>
+        <div class="close">X</div>
+      </div>
+      <div class="modal-body">
+        <div>
+        <label>Nhập tên khách hàng mới:</label>
+        <input type="text" id="nameInput" value="${user.fullName}" onChange="handleChangeValue(event, 'nameInput')">
+        </div>
+        <div>
+        <label>Nhập tên đăng nhập mới:</label>
+        <input type="text" id="usernameInput" value="${user.username}" onChange="handleChangeValue(event, 'usernameInput')">
+        </div>
+        <div>
+        <label>Nhập số điện thoại mới:</label>
+        <input type="text" id="phoneInput" value="${user.phone}" onChange="handleChangeValue(event, 'phoneInput')">
+        </div>
+        <div>
+        <label>Nhập email mới:</label>
+        <input type="email" id="emailInput" value="${user.email}" onChange="handleChangeValue(event, 'emailInput')">
+        </div>
+        <div>
+        <label>Nhập vai trò mới:</label>
+        <input type="text" id="roleInput" value="${user.role}" onChange="handleChangeValue(event, 'roleInput')">
+        </div>
+      <div>
+    <div class="modal-footer">
+    <button id="saveButton" onclick="updateUser(event, ${index})">Save</button>
+    </div>
+    </form>
+  `;
+
+  const closeButton = modalContent.querySelector('.close');
+
+  closeButton.addEventListener('click', closeModal);
+
+  modal.appendChild(modalContent);
+  document.body.appendChild(modal);
+  openModal();
+}
+
+function saveChanges() {
+  const nameInput = document.getElementById('nameInput').value;
+  const usernameInput = document.getElementById('usernameInput').value;
+  const phoneInput = document.getElementById('phoneInput').value;
+  const emailInput = document.getElementById('emailInput').value;
+  const roleInput = document.getElementById('roleInput').value;
+
   const user = listUsers[index];
-  const confirmation = confirm('Are you sure you want to edit this user?');
-  if (!confirmation) {
-    return;
-  }
-  else {
-    const name = prompt('Nhập tên khách hàng:', user.name);
-    const username = prompt('Nhập tên tài khoản:', user.username);
-    const phone = prompt('Nhập số điện thoại:', user.phone);
-    const email = prompt('Nhập email:', user.email);
-    const address = prompt('Nhập địa chỉ:', user.address);
-    const role = prompt('Nhập vai trò:', user.role);
+  user.fullName = nameInput;
+  user.username = usernameInput;
+  user.phone = phoneInput;
+  user.email = emailInput;
+  user.role = roleInput;
 
-    user.name = name;
-    user.username = username;
-    user.phone = phone;
-    user.email = email;
-    user.address = address;
-    user.role = role;
+  localStorage.setItem('List-users', JSON.stringify(listUsers));
 
-    localStorage.setItem('List-users', JSON.stringify(listUsers));
-    displayCustomerList();
-  }
-
+  closeModal();
+  displayCustomerList();
 }
