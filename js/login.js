0aa6b105b94919e5416537e92dd6b3b7888fd84d
Lâm Tài <lamtai@MacBook-Pro-cua-Lam.local>
2023-11-30 09:14:32 +0700

    Update link to admin managment

--- js/login.js
+++ js/login.js
@@ -1,124 +1,127 @@
 function login() {
-	let listUsers = localStorage.getItem('List-users') ? JSON.parse(localStorage.getItem('List-users')) : [];
-	let form = document.getElementById('form-login');
-	const overlay = document.getElementById('overlay');
-	const pop_up_login = document.querySelector(".pop-up-form-login");
-	const body = document.querySelector('body');
-	const blockUser = document.querySelector('.block-user .icon-user');
+  let listUsers = localStorage.getItem('List-users') ? JSON.parse(localStorage.getItem('List-users')) : [];
+  let form = document.getElementById('form-login');
+  const overlay = document.getElementById('overlay');
+  const pop_up_login = document.querySelector(".pop-up-form-login");
+  const body = document.querySelector('body');
+  const blockUser = document.querySelector('.block-user .icon-user');
 
-	function showMessageError(input, message) {
-		let parent = input.parentElement;
-		let form_message = parent.querySelector('.form-message')
-		parent.classList.add('invalid');
-		parent.classList.remove('success');
-		form_message.innerText = message;
-	}
+  function showMessageError(input, message) {
+	 let parent = input.parentElement;
+	 let form_message = parent.querySelector('.form-message')
+	 parent.classList.add('invalid');
+	 parent.classList.remove('success');
+	 form_message.innerText = message;
+  }
 
-	function showSuccess(input, message) {
-		let parent = input.parentElement;
-		let form_message = parent.querySelector('.form-message')
-		parent.classList.remove('invalid');
-		parent.classList.add('success');
-		form_message.innerText = '';
-	}
+  function showSuccess(input, message) {
+	 let parent = input.parentElement;
+	 let form_message = parent.querySelector('.form-message')
+	 parent.classList.remove('invalid');
+	 parent.classList.add('success');
+	 form_message.innerText = '';
+  }
 
-	form.addEventListener('submit', function (e) {
-		e.preventDefault();
-		let isLogin = false;
+  form.addEventListener('submit', function (e) {
+	 e.preventDefault();
+	 let isLogin = false;
 
-		let usernameDataInput = document.getElementById('username-login');
-		let passwordDataInput = document.getElementById('password-login');
+	 let usernameDataInput = document.getElementById('username-login');
+	 let passwordDataInput = document.getElementById('password-login');
 
-		for (let i = 0; i < listUsers.length; i++) {
+	 for (let i = 0; i < listUsers.length; i++) {
 
-			if (listUsers[i].username === usernameDataInput.value) {
-				showSuccess(usernameDataInput);
-			} else {
-				showMessageError(usernameDataInput, 'Tên đăng nhập không trùng khớp')
-			}
-
-			if (listUsers[i].password === passwordDataInput.value) {
-				showSuccess(passwordDataInput);
-			} else {
-				showMessageError(passwordDataInput, 'Mật khẩu không trùng khớp');
-			}
-
-			if (listUsers[i].username === usernameDataInput.value && listUsers[i].password === passwordDataInput.value) {
-				isLogin = true;
-				// console.log('UserID:', listUsers[i].userID);
-				localStorage.setItem('loggedInUsername', listUsers[i].username);
-				checkLogin();
-
-			} else {
-				console.log('dang nhap that bai')
-			}
+		if (listUsers[i].username === usernameDataInput.value) {
+		  showSuccess(usernameDataInput);
+		} else {
+		  showMessageError(usernameDataInput, 'Tên đăng nhập không trùng khớp')
 		}
-		// console.log(isLogin)
-		if (isLogin) {
-			form.reset();
-			localStorage.setItem('isLoggedIn', 'true');
-			body.classList.remove('no-scrollable');
 
-			const btnLogout = document.getElementById('btn-logout')
-			document.querySelector('body').classList.add('user-logged-in');
-			document.getElementById('btn-login').parentElement.style.display = 'none';
-			document.getElementById('btn-register').parentElement.style.display = 'none';
-			btnLogout.parentElement.style.display = 'block';
-			document.querySelector('.block-navigation-mobile .btn-login').parentElement.style.display = 'none';
-			document.querySelector('.block-navigation-mobile .btn-register').parentElement.style.display = 'none';
-			document.querySelector('.block-navigation-mobile .btn-logout').parentElement.style.display = 'block';
-			overlay.classList.remove('is-active');
-			pop_up_login.classList.remove('is-active');
-			location.reload();
+		if (listUsers[i].password === passwordDataInput.value) {
+		  showSuccess(passwordDataInput);
+		} else {
+		  showMessageError(passwordDataInput, 'Mật khẩu không trùng khớp');
+		}
+
+		if (listUsers[i].username === usernameDataInput.value && listUsers[i].password === passwordDataInput.value) {
+		  isLogin = true;
+		  // console.log('UserID:', listUsers[i].userID);
+		  localStorage.setItem('loggedInUsername', listUsers[i].username);
+		  checkLogin();
 
 		} else {
-			blockUser.innerText = '';
+		  console.log('dang nhap that bai')
 		}
-	})
+	 }
+	 // console.log(isLogin)
+	 if (isLogin) {
+		form.reset();
+		localStorage.setItem('isLoggedIn', 'true');
+		body.classList.remove('no-scrollable');
+
+		const btnLogout = document.getElementById('btn-logout')
+		document.querySelector('body').classList.add('user-logged-in');
+		document.getElementById('btn-login').parentElement.style.display = 'none';
+		document.getElementById('btn-register').parentElement.style.display = 'none';
+		btnLogout.parentElement.style.display = 'block';
+		document.querySelector('.block-navigation-mobile .btn-login').parentElement.style.display = 'none';
+		document.querySelector('.block-navigation-mobile .btn-register').parentElement.style.display = 'none';
+		document.querySelector('.block-navigation-mobile .btn-logout').parentElement.style.display = 'block';
+		overlay.classList.remove('is-active');
+		pop_up_login.classList.remove('is-active');
+		location.reload();
+
+	 } else {
+		blockUser.innerText = '';
+	 }
+  })
 }
 
 function checkLogin() {
-	const loggedInUsername = localStorage.getItem('loggedInUsername');
-	const blockUser = document.querySelector('.block-user .icon-user');
-	// const blockMobile = document.querySelector('.block-navigation-mobile')
+  const loggedInUsername = localStorage.getItem('loggedInUsername');
+  const blockUser = document.querySelector('.block-user .icon-user');
+  // const blockMobile = document.querySelector('.block-navigation-mobile')
+  let users = localStorage.getItem('List-users') ? JSON.parse(localStorage.getItem('List-users')) : [];
 
-	if (loggedInUsername) {
-		// Người dùng đã đăng nhập, bạn có thể sử dụng `loggedInUsername` để biết người dùng là ai.
-		console.log('Người đã đăng nhập:', loggedInUsername);
-		// Hiển thị tên đăng nhập hoặc thông tin người dùng trên giao diện.
-		blockUser.innerText = loggedInUsername;
-	} else {
-		// Người dùng chưa đăng nhập.
-		console.log('Chưa có người dùng đăng nhập.');
-	}
+  if (loggedInUsername) {
+	 // Người dùng đã đăng nhập, bạn có thể sử dụng `loggedInUsername` để biết người dùng là ai.
+	 console.log('Người đã đăng nhập:', loggedInUsername);
+	 // Hiển thị tên đăng nhập hoặc thông tin người dùng trên giao diện.
+	 blockUser.innerText = loggedInUsername;
+  } else {
+	 // Người dùng chưa đăng nhập.
+	 console.log('Chưa có người dùng đăng nhập.');
+	 document.querySelector('.menu-item-admin').style.display = 'none';
+
+  }
 }
 
 function logout() {
-	let listUsers = localStorage.getItem('List-users') ? JSON.parse(localStorage.getItem('List-users')) : [];
-	let productInCart = localStorage.getItem("Carts") ? JSON.parse(localStorage.getItem("Carts")) : [];
-	const blockUser = document.querySelector('.block-user .icon-user');
+  let listUsers = localStorage.getItem('List-users') ? JSON.parse(localStorage.getItem('List-users')) : [];
+  let productInCart = localStorage.getItem("Carts") ? JSON.parse(localStorage.getItem("Carts")) : [];
+  const blockUser = document.querySelector('.block-user .icon-user');
 
-	localStorage.setItem('isLoggedIn', 'false');
-	document.querySelector('body').classList.remove('user-logged-in');
-	document.getElementById('btn-login').parentElement.style.display = 'block';
-	document.getElementById('btn-register').parentElement.style.display = 'block';
-	document.getElementById('btn-logout').parentElement.style.display = 'none';
+  localStorage.setItem('isLoggedIn', 'false');
+  document.querySelector('body').classList.remove('user-logged-in');
+  document.getElementById('btn-login').parentElement.style.display = 'block';
+  document.getElementById('btn-register').parentElement.style.display = 'block';
+  document.getElementById('btn-logout').parentElement.style.display = 'none';
 
-	document.querySelector('.block-navigation-mobile .btn-login').parentElement.style.display = 'block';
-	document.querySelector('.block-navigation-mobile .btn-register').parentElement.style.display = 'block';
-	document.querySelector('.block-navigation-mobile .btn-logout').parentElement.style.display = 'none';
+  document.querySelector('.block-navigation-mobile .btn-login').parentElement.style.display = 'block';
+  document.querySelector('.block-navigation-mobile .btn-register').parentElement.style.display = 'block';
+  document.querySelector('.block-navigation-mobile .btn-logout').parentElement.style.display = 'none';
 
-	for (let i = 0; i < listUsers.length; i++) {
-		blockUser.innerText = '';
-		localStorage.setItem('loggedInUsername', '');
-	}
+  for (let i = 0; i < listUsers.length; i++) {
+	 blockUser.innerText = '';
+	 localStorage.setItem('loggedInUsername', '');
+  }
 
-	productInCart = [];
+  productInCart = [];
 
 
-	localStorage.setItem('Carts', JSON.stringify(productInCart));
-	calculatorQuantity();
-	renderProductToCart();
-	location.reload();
+  localStorage.setItem('Carts', JSON.stringify(productInCart));
+  calculatorQuantity();
+  renderProductToCart();
+  location.reload();
 }
 
--- js/search.js
+++ js/search.js
@@ -5,27 +5,26 @@ let currentPage = 1;
 let itemPerPage = 8;
 let totalPage = 0;
 let perProducts = storedProducts.slice(
-	(currentPage - 1) * itemPerPage,
-	(currentPage - 1) * itemPerPage + itemPerPage,
+  (currentPage - 1) * itemPerPage,
+  (currentPage - 1) * itemPerPage + itemPerPage,
 )
 
 
-
 function renderProductSearch(array) {
-	// totalPage = Math.ceil(array.length / itemPerPage);
-	// let perProducts = array.slice(
-	//  (currentPage - 1) * itemPerPage,
-	//  (currentPage - 1) * itemPerPage + itemPerPage,
-	// )
-	let products = document.getElementById('product-by-search');
-	// let formatVND = new Intl.NumberFormat("vi-VN", {
-	// 	style: "currency",
-	// 	currency: "VND",
-	// });
+  // totalPage = Math.ceil(array.length / itemPerPage);
+  // let perProducts = array.slice(
+  //  (currentPage - 1) * itemPerPage,
+  //  (currentPage - 1) * itemPerPage + itemPerPage,
+  // )
+  let products = document.getElementById('product-by-search');
+  // let formatVND = new Intl.NumberFormat("vi-VN", {
+  // 	style: "currency",
+  // 	currency: "VND",
+  // });
 
-	let data = ``;
-	array.map((productArray) => {
-		data += `
+  let data = ``;
+  array.map((productArray) => {
+	 data += `
 		<div class="views-row">
 			<div class="view-row-content" >
 			  <div class="view-field-image" >
@@ -51,160 +50,161 @@ function renderProductSearch(array) {
 		</div>
 		
 	 	`;
-	});
-	products.querySelector('.view-content-wrapper').innerHTML = data;
-	// renderPagination(perProducts);
+  });
+  products.querySelector('.view-content-wrapper').innerHTML = data;
+  // renderPagination(perProducts);
 }
 
 function renderPagination(array) {
-	totalPage = Math.ceil(array.length / itemPerPage);
-	const wrapper = document.getElementById('product-by-search')
-	const pagination = document.createElement('ul')
-	pagination.classList.add('custom-pagination')
-	wrapper.appendChild(pagination)
-	for (let i = 1; i <= totalPage; i++) {
-		// pagination.innerHTML += `<li class="pagination-item" onclick="handlePageNumber(${i})">${i}</li>`
-		let newPage = document.createElement('li')
-		newPage.classList.add('pagination-item')
-		newPage.innerText = i;
-		if (i === currentPage) {
-			newPage.classList.add('is-active');
-		}
-		// newPage.setAttribute('onclick', "handlePageNumber(" + i + ")");
-		newPage.addEventListener('click', function () {
-			handlePageNumber(i, array);
-			highlightActivePage(i);
-		});
-		pagination.appendChild(newPage);
-	}
+  totalPage = Math.ceil(array.length / itemPerPage);
+  const wrapper = document.getElementById('product-by-search')
+  const pagination = document.createElement('ul')
+  pagination.classList.add('custom-pagination')
+  wrapper.appendChild(pagination)
+  for (let i = 1; i <= totalPage; i++) {
+	 // pagination.innerHTML += `<li class="pagination-item" onclick="handlePageNumber(${i})">${i}</li>`
+	 let newPage = document.createElement('li')
+	 newPage.classList.add('pagination-item')
+	 newPage.innerText = i;
+	 if (i === currentPage) {
+		newPage.classList.add('is-active');
+	 }
+	 // newPage.setAttribute('onclick', "handlePageNumber(" + i + ")");
+	 newPage.addEventListener('click', function () {
+		handlePageNumber(i, array);
+		highlightActivePage(i);
+	 });
+	 pagination.appendChild(newPage);
+  }
 }
 
 function handlePageNumber(num, array) {
-	currentPage = num
-	perProducts = array.slice(
-		(currentPage - 1) * itemPerPage,
-		(currentPage - 1) * itemPerPage + itemPerPage,
-	)
-	renderProductSearch(perProducts);
+  currentPage = num
+  perProducts = array.slice(
+	 (currentPage - 1) * itemPerPage,
+	 (currentPage - 1) * itemPerPage + itemPerPage,
+  )
+  renderProductSearch(perProducts);
 }
 
 function highlightActivePage(pageNumber) {
-	const paginationItems = document.querySelectorAll('.pagination-item');
-	paginationItems.forEach(item => {
-		if (parseInt(item.innerText) === pageNumber) {
-			item.classList.add('is-active');
-		} else {
-			item.classList.remove('is-active');
-		}
-	});
+  const paginationItems = document.querySelectorAll('.pagination-item');
+  paginationItems.forEach(item => {
+	 if (parseInt(item.innerText) === pageNumber) {
+		item.classList.add('is-active');
+	 } else {
+		item.classList.remove('is-active');
+	 }
+  });
 }
 
 function searchByTitle() {
-	let valueSearch = inputSearch.value;
-	let userSearch = storedProducts.filter(value => {
-		return value.name.toUpperCase().includes(valueSearch.toUpperCase());
-	})
-	window.location.href = `search.html?query=${encodeURIComponent(valueSearch)}`;
-	// console.log(encodeURIComponent(valueSearch))
-	renderProductSearch(userSearch);
-	// renderPagination(userSearch)
+  let valueSearch = inputSearch.value;
+  let userSearch = storedProducts.filter(value => {
+	 return value.name.toUpperCase().includes(valueSearch.toUpperCase());
+  })
+  window.location.href = `search.html?query=${encodeURIComponent(valueSearch)}`;
+  // console.log(encodeURIComponent(valueSearch))
+  renderProductSearch(userSearch);
+  // renderPagination(userSearch)
 }
 
 function searchLoadPage() {
-	const btnSearch = document.getElementById('btn-search')
-
-	inputSearch.addEventListener('keydown', function (e) {
+  const btnSearch = document.getElementById('btn-search')
+  if (btnSearch !== null) {
+	 inputSearch.addEventListener('keydown', function (e) {
 		if (e.key === 'Enter') {
-			e.preventDefault()
-			searchByTitle();
+		  e.preventDefault()
+		  searchByTitle();
 		}
-	});
+	 });
 
-	btnSearch.addEventListener('click', function () {
+	 btnSearch.addEventListener('click', function () {
 		searchByTitle();
-	});
+	 });
+  }
 }
 
 function applyFilter(array) {
-	let form = document.querySelector('.filter-search');
-	form.addEventListener('submit', function (e) {
-		e.preventDefault();
+  let form = document.querySelector('.filter-search');
+  form.addEventListener('submit', function (e) {
+	 e.preventDefault();
 
-		const wrapper = document.getElementById('product-by-search');
-		const existingPagination = wrapper.querySelector('.custom-pagination');
-		if (existingPagination) {
-			wrapper.removeChild(existingPagination);
+	 const wrapper = document.getElementById('product-by-search');
+	 const existingPagination = wrapper.querySelector('.custom-pagination');
+	 if (existingPagination) {
+		wrapper.removeChild(existingPagination);
+	 }
+
+	 let valueFilter = e.target.elements;
+	 // console.log(valueFilter)
+	 let productFilter = array.filter(item => {
+		let literature = item.categories === 'Văn học';
+		let childrenBook = item.categories === 'Thiếu nhi';
+		let textbook = item.categories === 'Sách giáo khoa';
+		let foreignLanguageBook = item.categories === 'Sách ngoại ngữ';
+
+		if (valueFilter.category.value !== '') {
+		  if (item.categories !== valueFilter.category.value) {
+			 return false;
+		  }
 		}
 
-		let valueFilter = e.target.elements;
-		// console.log(valueFilter)
-		let productFilter = array.filter(item => {
-			let literature = item.categories === 'Văn học';
-			let childrenBook = item.categories === 'Thiếu nhi';
-			let textbook = item.categories === 'Sách giáo khoa';
-			let foreignLanguageBook = item.categories === 'Sách ngoại ngữ';
+		if (valueFilter.minPrice.value !== '') {
+		  if (item.price < valueFilter.minPrice.value) {
+			 return false;
+		  }
+		}
 
-			if (valueFilter.category.value !== '') {
-				if (item.categories !== valueFilter.category.value) {
-					return false;
-				}
-			}
+		if (valueFilter.maxPrice.value !== '') {
+		  if (item.price > valueFilter.maxPrice.value) {
+			 return false;
+		  }
+		}
 
-			if (valueFilter.minPrice.value !== '') {
-				if (item.price < valueFilter.minPrice.value) {
-					return false;
-				}
-			}
-
-			if (valueFilter.maxPrice.value !== '') {
-				if (item.price > valueFilter.maxPrice.value) {
-					return false;
-				}
-			}
-
-			if (valueFilter.minPrice.value !== '' && valueFilter.maxPrice.value !== '') {
-				if (item.price < valueFilter.minPrice.value && item.price > valueFilter.maxPrice.value) {
-					return false
-				}
-			}
+		if (valueFilter.minPrice.value !== '' && valueFilter.maxPrice.value !== '') {
+		  if (item.price < valueFilter.minPrice.value && item.price > valueFilter.maxPrice.value) {
+			 return false
+		  }
+		}
 
 
-			// console.log(item.categories)
-			return true;
-		})
-		console.log(productFilter)
-		let perProducts = productFilter.slice(
-			(currentPage - 1) * itemPerPage,
-			(currentPage - 1) * itemPerPage + itemPerPage,
-		)
-		renderProductSearch(perProducts);
-		renderPagination(productFilter);
-		count.innerText = productFilter.length;
-	})
+		// console.log(item.categories)
+		return true;
+	 })
+	 console.log(productFilter)
+	 let perProducts = productFilter.slice(
+		(currentPage - 1) * itemPerPage,
+		(currentPage - 1) * itemPerPage + itemPerPage,
+	 )
+	 renderProductSearch(perProducts);
+	 renderPagination(productFilter);
+	 count.innerText = productFilter.length;
+  })
 }
 
 // Check if the current page is search.html and render results accordingly
 if (window.location.pathname.includes('search.html')) {
-	const searchQuery = new URLSearchParams(window.location.search).get('query');
-	// const item = document.querySelectorAll('.category')
-	if (searchQuery) {
-		inputSearch.value = decodeURIComponent(searchQuery);
-		let valueSearch = storedProducts.filter(value => value.name.toUpperCase().includes(inputSearch.value.toUpperCase()));
-		let perProducts = valueSearch.slice(
-			(currentPage - 1) * itemPerPage,
-			(currentPage - 1) * itemPerPage + itemPerPage,
-		)
-		renderProductSearch(perProducts)
-		count.innerText = valueSearch.length;
-		renderPagination(valueSearch)
-		applyFilter(valueSearch)
+  const searchQuery = new URLSearchParams(window.location.search).get('query');
+  // const item = document.querySelectorAll('.category')
+  if (searchQuery) {
+	 inputSearch.value = decodeURIComponent(searchQuery);
+	 let valueSearch = storedProducts.filter(value => value.name.toUpperCase().includes(inputSearch.value.toUpperCase()));
+	 let perProducts = valueSearch.slice(
+		(currentPage - 1) * itemPerPage,
+		(currentPage - 1) * itemPerPage + itemPerPage,
+	 )
+	 renderProductSearch(perProducts)
+	 count.innerText = valueSearch.length;
+	 renderPagination(valueSearch)
+	 applyFilter(valueSearch)
 
-	} else {
-		renderProductSearch(perProducts);
-		count.innerText = storedProducts.length;
-		applyFilter(storedProducts)
-		renderPagination(storedProducts);
-	}
+  } else {
+	 renderProductSearch(perProducts);
+	 count.innerText = storedProducts.length;
+	 applyFilter(storedProducts)
+	 renderPagination(storedProducts);
+  }
 }
 
 
